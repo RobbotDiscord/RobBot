f84b61e2ef8cc0206042e45cbc445e5f4bd964fe
robigan <35210888+robigan@users.noreply.github.com>
2021-12-22 09:33:00 +1100

    God help me

---
+++ instances/gateway/src/structures/GatewayClient.ts
@@ -0,0 +1,50 @@
+// /**
+//  * GatewayClient, the entrypoint for initiating a gateway client
+//  * @module gateway/client
+//  */
+
+// const CloudStorm = require("cloudstorm");
+
+// module.exports = class GatewayClient extends CloudStorm {
+//     /**
+//      * Initiate the gateway service for RobBot
+//      * @constructor
+//      * @param {object} Config
+//      * @param {import("../../amqp/AmqpClient")} AmqpClient
+//      */
+//     constructor(Config, AmqpClient) {
+//         super(Config.token, Config.CloudStorm);
+//         this.Config = Config;
+//         this.Interface = this.Config.amqp.queueGatewayCache;
+//         this.AmqpClient = AmqpClient;
+
+//         super.on("error", console.error);
+
+//         this.Config.debug.gateway ? super.on("debug", console.warn) : undefined;
+
+//         super.once("ready", async () => {
+//             console.log("Gateway : Logged in!");
+//         });
+//     }
+
+//     /**
+//      * Start function
+//      * @async
+//      * @function
+//      */
+//     async start() {
+//         this.Channel = await this.AmqpClient.initQueue(this.Interface);
+//         //this.Channel.sendToQueue(this.Interface, Buffer.from("Hello World"));
+//         super.connect();
+
+//         super.on("event", async (event) => {
+//             this.Channel.sendToQueue(this.Interface, Buffer.from(JSON.stringify(Object.assign(event, {
+//                 "stats": {
+//                     "gatewayPID": process.pid
+//                 }
+//             }))));
+//         });
+//     }
+// };
+
+const { Client } = require("cloudstorm");
