3ea23f58f826a89021da7d7c087380caf647a353
robigan <35210888+robigan@users.noreply.github.com>
2021-12-26 17:49:27 +1100

    Many changes and it's a bad behavior

--- instances/gateway/src/GatewayClient.ts
+++ instances/gateway/src/GatewayClient.ts
@@ -1,8 +1,16 @@
 import { Client } from "cloudstorm";
 import { ConfigClient, DotenvConfigEngine } from "config";
+import { AmqpWrapperConnection } from "amqp-wrapper";
+import debugModule from "debug";
+
+const log = debugModule("gateway:log");
+log.log = console.log.bind(console);
+const error = debugModule("gateway:error");
 
 class GatewayClient extends Client {
-    constructor(configClient: ConfigClient) {
+    private amqpWrapper: AmqpWrapperConnection;
+
+    constructor(configClient: ConfigClient, amqpWrapper: AmqpWrapperConnection) {
         configClient.initialize("gateway");
         if (configClient.data["common"]?.token === undefined) {
             throw new Error("Token hasn't been defined");
@@ -10,18 +18,21 @@ class GatewayClient extends Client {
 
         super(configClient.data["common"].token, configClient.data.gateway);
 
-        super.on("error", console.error);
+        this.amqpWrapper = amqpWrapper;
+
+        super.on("error", error);
     }
 
     public async start() {
         super.on("event", async (event) => {
-            console.log(event);
+            log("%O", event);
         });
     }
 }
 
 const dotenvConfigEngine = new DotenvConfigEngine("/etc/robbot.env");
 const configClient = new ConfigClient(dotenvConfigEngine);
-const gatewayClient = new GatewayClient(configClient);
+const amqpWrapper = new AmqpWrapperConnection(configClient);
+const gatewayClient = new GatewayClient(configClient, amqpWrapper);
 
 gatewayClient.start();
--- packages/amqp-wrapper/src/index.ts
+++ packages/amqp-wrapper/src/index.ts
@@ -1,12 +1,32 @@
 import { ConfigClient } from "config";
-import { Connection, Receiver, ReceiverOptions, Sender, SenderOptions } from "rhea-promise";
+import { Connection, ReceiverOptions, SenderOptions, ConnectionEvents, SenderEvents, ReceiverEvents } from "rhea-promise";
+import debugModule from "debug";
 
-class AmqpWrapper extends Connection {
+const log = debugModule("amqp-wrapper:log");
+log.log = console.log.bind(console);
+const logConnection = log.extend("connection");
+const logSender = log.extend("sender");
+const logReceiver = log.extend("receiver");
+const error = debugModule("amqp-wrapper:error");
+const errorConnection = error.extend("connection");
+const errorSender = error.extend("sender");
+const errorReceiver = error.extend("receiver");
+debugModule.enable("amqp-wrapper:error:*");
+
+class AmqpWrapperConnection extends Connection {
     // private configClient: ConfigClient;
 
     constructor(configClient: ConfigClient) {
         super(configClient.data.amqp);
         // this.configClient = configClient;
+
+        super.on(ConnectionEvents.connectionError, async (context) => {
+            errorConnection("Connection error\n%j", context.error ?? {"message": "No error obj provided"});
+        });
+
+        super.on(ConnectionEvents.connectionClose, async () => {
+            logConnection("Connection closed");
+        });
     }
 
     public async start() {
@@ -14,16 +34,30 @@ class AmqpWrapper extends Connection {
     }
 
     public async initSender(senderOptions: SenderOptions) {
-        const sender: Promise<Sender> = super.createSender(senderOptions);
+        const sender = await super.createSender(senderOptions);
 
-        return await sender;
+        sender.on(SenderEvents.senderError, async (context) => {
+            errorSender("Sender error\n%j", context.error ?? {"message": "No error obj provided"});
+        });
+        sender.on(SenderEvents.senderClose, async () => {
+            logSender("Sender closed: %s", sender.name);
+        });
+
+        return sender;
     }
 
     public async initReceiver(receiverOptions: ReceiverOptions) {
-        const receiver: Promise<Receiver> = super.createReceiver(receiverOptions);
+        const receiver = await super.createReceiver(receiverOptions);
 
-        return await receiver;
+        receiver.on(ReceiverEvents.receiverError, async (context) => {
+            errorReceiver("Receiver error\n%j", context.error ?? {"message": "No error obj provided"});
+        });
+        receiver.on(ReceiverEvents.receiverClose, async () => {
+            logReceiver("Receiver closed: %s", receiver.name);
+        });
+
+        return receiver;
     }
 }
 
-export { AmqpWrapper };
+export { AmqpWrapperConnection };
